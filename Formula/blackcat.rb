4f4cf82efe55d0024606782815477376ed2c3589
Jesse Miller <jmiller@jmiller.com>
2026-05-03 14:15:46 -0600

    Add blackcat 0.4.5 (new formula)
    Closes #1.
    
    Signed-off-by: Jesse Miller <jmiller@jmiller.com>

---
+++ Formula/blackcat.rb
@@ -0,0 +1,20 @@
+class Blackcat < Formula
+  desc "Modern cat with Kitty graphics protocol, ANSI rendering, DOS/CP437 support"
+  homepage "https://github.com/j-c-m/blackcat"
+  url "https://github.com/j-c-m/blackcat/archive/refs/tags/v0.4.5.tar.gz"
+  sha256 "2e7193d5c0ab7eb04ae72c03df8f07212db42e86d67558af6a5fe047329c91b8"
+  license "MIT"
+
+  depends_on "zig@0.15" => :build
+
+  def install
+    system "zig", "build", "-Doptimize=ReleaseSmall", "-Dpackage_release=false"
+    bin.install "zig-out/bin/blackcat"
+  end
+
+  test do
+    assert_equal "hello\n", pipe_output(bin/"blackcat", "hello\n")
+    assert_match version.to_s, shell_output("#{bin}/blackcat --version")
+    assert_match "Kitty protocol", shell_output("#{bin}/blackcat --help")
+  end
+end
