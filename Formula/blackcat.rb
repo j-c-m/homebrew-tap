705c165fc20c2e462a9c22774c6bb55515fb7bc1
Jesse Miller <jmiller@jmiller.com>
2026-05-03 20:22:43 +0000

    blackcat: add 0.4.5 bottle.

--- Formula/blackcat.rb
+++ Formula/blackcat.rb
@@ -5,6 +5,12 @@ class Blackcat < Formula
   sha256 "2e7193d5c0ab7eb04ae72c03df8f07212db42e86d67558af6a5fe047329c91b8"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/j-c-m/homebrew-tap/releases/download/blackcat-0.4.5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "285ed38128cc40915579f1e6879fadae9ef7e0e75ebdcde9466d38a74dfe273b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "db5d59711ffa26343b982035058ab8f3ed3109f4208142bb8203d5df5194b626"
+  end
+
   depends_on "zig@0.15" => :build
 
   def install
