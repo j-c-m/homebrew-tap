704d027a22be9a31f8ae554126eb9a9153c51269
Jesse Miller <jmiller@jmiller.com>
2026-09-23 23:11:56 +0000

    blackcat: update 0.9.0 bottle.

--- Formula/blackcat.rb
+++ Formula/blackcat.rb
@@ -6,9 +6,9 @@ class Blackcat < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/j-c-m/homebrew-tap/releases/download/blackcat-0.8.3"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "9707dbbf36a6a8584bed5178879be9c084dd9b9f31e2928dbf4c6d50fb0f8c30"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9f0bbdb8255688cf5ef8df15f09c224a5c5bf432e05488ef0879749b65ef9b6b"
+    root_url "https://github.com/j-c-m/homebrew-tap/releases/download/blackcat-0.9.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "14102553509e13bbb268bd8e850fb2a51e761416caab851501f09d5869ccc729"
+    sha256 cellar: :any,                 x86_64_linux: "7d503c757e3d19db8fb74010be38220afb97df6238fe16860c22099b5739f337"
   end
 
   depends_on "rust" => :build
